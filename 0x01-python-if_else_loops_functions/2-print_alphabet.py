6634d6e1d79f8a4d1a8849144df93e80fa452925
Blackpytz <theophiluschikezie@gmail.com>
2024-01-04 01:57:50 +0100

    Update file

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# ASCII value for 'a'
-alphabet = ord('a')
 
 # Loop throught the lowercase alphabet ASCII value
-for i in range(26):
-    print("{}".format(chr(alphabet + i)), end='')
+for i in range(97, 123):
+    print("{}".format(chr(i)), end='')
