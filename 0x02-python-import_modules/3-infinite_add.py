a92badfc0fd16f013481842218e388c0d82b73b3
Blackpytz <theophiluschikezie@gmail.com>
2024-01-05 09:54:05 +0100

    Add 3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+arg_len = len(argv)
+if (arg_len == 1):
+    print("0")
+else:
+    sum = 0
+    for i in range(1, arg_len):
+        sum += int(argv[i])
+    print("{}".format(sum))
