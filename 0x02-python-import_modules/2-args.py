a5b928f31ad29ff4da12ffdc7353d2325a28eab2
Blackpytz <theophiluschikezie@gmail.com>
2024-01-05 09:38:09 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
+from sys import argv
 
-arg_len = len(argv)
-if arg_len == 1:
-    print("0 arguments")
-else:
-    print("{} arguments.".format(int(arg_len) - 1))
-    for i in range(1, arg_len):
-        print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    arg_len = len(argv)
+    if arg_len == 1:
+        print("0 arguments")
+    else:
+        print("{} arguments:".format(int(arg_len) - 1))
+        for i in range(1, arg_len):
+            print("{}: {}".format(i, argv[i]))
