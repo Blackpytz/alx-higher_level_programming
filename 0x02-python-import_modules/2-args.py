381e89483d1e7364b49efca380885cc4a537a5d4
Blackpytz <theophiluschikezie@gmail.com>
2024-01-05 09:42:46 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,10 @@ from sys import argv
 if __name__ == "__main__":
     arg_len = len(argv)
     if arg_len == 1:
-        print("0 arguments")
+        print("0 arguments.")
+    elif arg_len == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(int(arg_len) - 1))
         for i in range(1, arg_len):
