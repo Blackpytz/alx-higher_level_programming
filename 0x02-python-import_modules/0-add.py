d965a4421d4ecbc3e18d83161eb7c122865c5cdb
Blackpytz <theophiluschikezie@gmail.com>
2024-01-05 07:56:41 +0100

    Add .gitignore file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def add(a, b):
-    print("{} + {} = {}".format(a, b, a + b))
 
+a = 1
+b = 2
 
-add(1, 2)
+from add_0 import add
+
+result = add(a, b)
+print(f"{a} + {b} = {result}")
