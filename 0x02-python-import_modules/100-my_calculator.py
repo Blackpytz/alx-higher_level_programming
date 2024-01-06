3ed08af27bf3a8a49491a045b7edb2624264115f
Blackpytz <theophiluschikezie@gmail.com>
2024-01-06 22:17:40 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    argument_count = len(sys.argv)
+    if argument_count != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    operator = sys.argv[2]
+    print("{}".format(operator))
+    if operator != '+' and '-' and '*' and '/':
+        print("Unknown operator. Available operators: +, -, * and /")
