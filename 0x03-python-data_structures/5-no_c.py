0fdfe8e49b83bed2298de6b4270d86e6c16c85ed
Blackpytz <theophiluschikezie@gmail.com>
2024-01-09 00:01:40 +0100

    Add 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string:
+        copy_list = ''
+
+        for i in my_string:
+            if i != 'c' and i != 'C':
+                copy_list += i
+
+        return (copy_list)
