5768998c02ffc6845ecebcf898a3865ae4c7b7e4
Blackpytz <theophiluschikezie@gmail.com>
2024-01-25 22:04:06 +0100

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A module of a class Square that defines a square by:
+
+    * Private instance attribute: size
+    * Instantiation with size(no type/value verification)
+"""
+
+
+class Square:
+    """Not allowed to import any module"""
+    def __init__(self, size):
+        self.__size = size
