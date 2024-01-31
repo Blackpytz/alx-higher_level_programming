8c53f6d06e2b94b7e128aa11d34638943008535e
Blackpytz <theophiluschikezie@gmail.com>
2024-01-31 14:58:38 +0100

    Update 2-rectangle.py file

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,48 +3,44 @@
 
 
 class Rectangle:
-    """Instantiate the class with optional width and height"""
+    """Define a rectangle"""
     def __init__(self, width=0, height=0):
+        """Initialize the rectangle"""
         self.width = width
         self.height = height
 
-    """Use the @property decorator to transform the width attribute
-        into a read-only property"""
     @property
     def width(self):
+        """Retrieve the width"""
         return self.__width
 
-    """Use the @setter decorator to transform the width attribute
-        into a setter method"""
     @width.setter
     def width(self, value):
+        """Set the height with type and value checks"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    """Use the @property decorator to transform the height attribute
-        into a read-only property"""
     @property
     def height(self):
+        """Retrieve the height"""
         return self.__height
 
-    """Use the @setter decorator to transform the height attribute
-        into a setter method"""
     @height.setter
     def height(self, value):
+        """Set the height with type and value checks"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    """Public instance method that returns the rectangle area"""
     def area(self):
+        """Calculate and returns the area of the rectangle area"""
         return self.__width * self.__height
 
-    """Public instance method that returns the rectangle perimeter"""
     def perimeter(self):
-        p = (self.__height + self.__width) * 2
-        return p
+        """Calculate and returns the perimeter of the rectangle perimeter"""
+        return 2 * (self.__height + self.__width)
