161c28227bc56f7fda41f59a2245a67fe1c6d3b0
Blackpytz <theophiluschikezie@gmail.com>
2024-01-31 14:52:06 +0100

    Update 2-rectangle.py file

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    
+
     """Public instance method that returns the rectangle area"""
     def area(self):
         return self.__width * self.__height
