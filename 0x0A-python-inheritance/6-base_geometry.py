110348fe282ba97bfd839c462389a5f7e43e6413
Blackpytz <theophiluschikezie@gmail.com>
2024-02-06 12:02:28 +0100

    new file:   0x0A-python-inheritance/6-base_geometry.py new file:   0x0A-python-inheritance/__pycache__/6-base_geometry.cpython-38.pyc

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module: 5-base_geometry.py
+
+Contains an empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Defines a class called BaseGeometry"""
+    def area(aelf):
+        raise Exception("area() is not implemented")
