aa8a492de1eeffb6c2f07288eef275313dbb82f8
Blackpytz <theophiluschikezie@gmail.com>
2024-02-06 15:54:52 +0100

    new file: 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module: 8-rectangle.py
+
+Contains a class Square that inherits from BaseGeometry
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a Square class"""
+    def __init__(self, size):
+        """
+        Initializes a square object with size attribute
+
+        Args:
+            size (int): The size of the square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        """
+        Call the parent class constructor
+        with size as both width and height
+        """
+        self.__size = size
+
+    def __str__(self):
+        return '[Square] {}/{}'.format(self.__size, self.__size)
