d2c821fa945884a9a9f5038b1a13b8faf1a3d9f7
Blackpytz <theophiluschikezie@gmail.com>
2024-02-06 11:34:32 +0100

    new file:   0x0A-python-inheritance/3-is_kind_of_class.py new file:   0x0A-python-inheritance/__pycache__/3-is_kind_of_class.cpython-38.pyc

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module: 3-is_kind_of_class.py
+
+Contains a function with that checks if the object is
+an instance of, or if the object is an instance of a
+class that inherited from, the specified class
+"""
+def is_kind_of_class(obj, a_class):
+    """Returns True of False"""
+    if isinstance(obj, a_class):
+        return True
+    return False
