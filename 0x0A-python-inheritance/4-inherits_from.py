79d352d6b97999309c1c17cb54f91e85af522de1
Blackpytz <theophiluschikezie@gmail.com>
2024-02-06 11:42:47 +0100

    new file:   0x0A-python-inheritance/4-inherits_from.py new file:   0x0A-python-inheritance/__pycache__/4-inherits_from.cpython-38.pyc

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module: 3-is_kind_of_class.py
+
+Contains a function with that checks if the object is
+an instance of, or if the object is an instance of a
+class that inherited (directly or indirectly)from, the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True of False"""
+    return (type(obj) != a_class)
