5fed4c17ab6417a27ae0b54a3ae9f32392631b26
Blackpytz <theophiluschikezie@gmail.com>
2024-02-08 20:32:41 +0100

    modified:   0x0B-python-input_output/__pycache__/5-save_to_json_file.cpython-38.pyc

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -16,6 +16,5 @@ def save_to_json_file(my_obj, filename):
         my_obj (str): A JSON serializable object
         filename: name of the text file
     """
-    obj = json.dumps(my_obj)
     with open(filename, 'w', encoding='utf8') as f:
-        f.write(obj)
+        json.dump(my_obj, f)
