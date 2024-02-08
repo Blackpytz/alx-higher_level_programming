0cf3509533c975e5e4eab2de0468a5bdaa2317cd
Blackpytz <theophiluschikezie@gmail.com>
2024-02-08 19:17:26 +0100

    new file:   0x0B-python-input_output/4-from_json_string.py new file:   0x0B-python-input_output/__pycache__/4-from_json_string.cpython-38.pyc

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module: from_json_string
+
+Contains a function that returns an object (Python data
+structure) represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Defines a function takes a single argument
+    and returns a JSON string representation
+    
+    Args:
+        my_str: A JSON serializable object
+    """
+    return json.loads(my_str)
