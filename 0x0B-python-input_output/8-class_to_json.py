52808a9768648c75706b79c42282965dff3704d4
Blackpytz <theophiluschikezie@gmail.com>
2024-02-09 12:36:34 +0100

    new file:   0x0B-python-input_output/8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module: 8-class_to_json
+
+Contains a function that returns the dictionary description
+with simple data structure for JSON serialization of an object.
+"""
+
+import json
+
+
+def class_to_json(obj):
+    """
+    Defined a function that takes a single argument
+
+    Args:
+        obj : An instance of a Class
+    """
+    return json.dumps(obj.__dict__)
