757630b29039fa9b67f7e98324ade968bd27613f
Blackpytz <theophiluschikezie@gmail.com>
2024-02-12 11:01:03 +0100

    modified:   0x0C-python-almost_a_circle/models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,7 +27,14 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None or list_dictionaries == {}:
-            return "[]"
+        """
+        Returns the JSON string representation of list_dictionaries
+        Otherwise return the string "[]"
 
-        return json.dump(list_dictionaries)
+        Args:
+            list_dictionaries (list): A list of dictionaries
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
