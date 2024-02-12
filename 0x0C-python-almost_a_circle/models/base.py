b79440fe317e82450334d2ce917f81b5ad220f49
Blackpytz <theophiluschikezie@gmail.com>
2024-02-12 12:00:12 +0100

    modified:   0x0C-python-almost_a_circle/models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -53,3 +53,15 @@ class Base:
             else:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 jsonfile.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string Otherwise an empty list
+
+        Args:
+            json_string (json string): a list
+        """
+        if json_string is None:
+            return "[]"
+        return json.loads(json_string)
