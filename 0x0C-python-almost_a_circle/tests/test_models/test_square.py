700a08b23ec5f7c054fb0b8adf216c39bef95a23
Blackpytz <theophiluschikezie@gmail.com>
2024-02-15 19:12:26 +0100

    modified:   0x0C-python-almost_a_circle/tests/test_models/test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -3,7 +3,7 @@
 Defines unittest for models/square.py
 
 Unittest classes:
-    TestSquare_instantiation - line 24
+    TestSquare_instantiation - line 23
     TestSquare_size - line 88
     TestSquare_x - line 166
     TestSquare_y - line 238
@@ -21,4 +21,13 @@ from models.base import Base
 from models.square import Square
 
 class TestSquare_instantiation(unittest.TestCase):
-    
+    """Unittest for testing instantiation of the Square class."""
+
+    def test_is_base(self):
+        self.assertIsInstance(Square(10), Base)
+
+    def test_is_rectangle(self):
+        self.assertIsInstance(Square(10), Square)
+
+if __name__ == "__main__":
+    unittest.main()
