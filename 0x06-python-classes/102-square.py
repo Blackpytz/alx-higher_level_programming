046add95e1f1fe89c31800842692247c6dc89c23
Blackpytz <theophiluschikezie@gmail.com>
2024-02-17 13:02:11 +0100

    new file:   0x06-python-classes/102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Module 102-square
+
+Contains a class Square
+"""
+
+
+class Square:
+    """Defines the Square class"""
+    def __init__(self, size=0):
+        """
+        Initializes the square object with a single parameter
+
+        Args:
+            size (int): size of the square object
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
